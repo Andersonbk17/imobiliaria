5283832e06fc6a606bec4c22e5849ac936cc11ac
emerson <emerson@emerson-PC>
2013-11-25 21:15:29 -0200

    Criação classe Funcionario

---
+++ imobiliaria/src/java/br/edu/ifnmg/imobiliaria/domainModel/Funcionario.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.imobiliaria.domainModel;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author emerson
+ */
+@Entity
+public class Funcionario extends Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataAdmissao;
+    
+    @ManyToOne
+    private Cargo cargo;
+
+    public Funcionario() {
+        this.dataAdmissao = new Date();
+        this.cargo = new Cargo();
+    }
+    
+      
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(Date dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+    
+
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Funcionario)) {
+            return false;
+        }
+        Funcionario other = (Funcionario) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.edu.ifnmg.imobiliaria.domainModel.Funcionario[ id=" + id + " ]";
+    }
+    
+}
