0c067c3921e4b1f0424a7c73fcace207316edb6f
Anderson <Anderson@darkside>
2013-11-25 22:56:50 -0200

    Criacao da Classe Visitas

---
+++ imobiliaria/src/java/br/edu/ifnmg/imobiliaria/domainModel/Visita.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.imobiliaria.domainModel;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Anderson
+ */
+@Entity
+public class Visita implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Cliente cliente;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Calendar dataAgenda;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Calendar dataRegistro;
+    @ManyToOne
+    private Imovel imovel;
+
+    public Visita() {
+        this.cliente = null;
+        this.dataAgenda = null;
+        this.dataRegistro.setTime(new Date());
+        this.imovel = null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Calendar getDataAgenda() {
+        return dataAgenda;
+    }
+
+    public void setDataAgenda(Calendar dataAgenda) {
+        this.dataAgenda = dataAgenda;
+    }
+
+    public Calendar getDataRegistro() {
+        return dataRegistro;
+    }
+
+    public void setDataRegistro(Calendar dataRegistro) {
+        this.dataRegistro = dataRegistro;
+    }
+
+    public Imovel getImovel() {
+        return imovel;
+    }
+
+    public void setImovel(Imovel imovel) {
+        this.imovel = imovel;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 13 * hash + Objects.hashCode(this.cliente);
+        hash = 13 * hash + Objects.hashCode(this.dataAgenda);
+        hash = 13 * hash + Objects.hashCode(this.dataRegistro);
+        hash = 13 * hash + Objects.hashCode(this.imovel);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Visita other = (Visita) obj;
+        if (!Objects.equals(this.cliente, other.cliente)) {
+            return false;
+        }
+        if (!Objects.equals(this.dataAgenda, other.dataAgenda)) {
+            return false;
+        }
+        if (!Objects.equals(this.dataRegistro, other.dataRegistro)) {
+            return false;
+        }
+        if (!Objects.equals(this.imovel, other.imovel)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Visita{" + "id=" + id + ", cliente=" + cliente + ", dataAgenda=" + dataAgenda + ", dataRegistro=" + dataRegistro + ", imovel=" + imovel + '}';
+    }
+    
+    
+    
+}
