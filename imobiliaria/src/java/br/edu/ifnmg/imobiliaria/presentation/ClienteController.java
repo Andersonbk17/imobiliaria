b67b53d9116981dcbbc1de23bc88fb60f86b41d3
emerson <emerson@emerson-PC>
2013-11-26 17:50:26 -0200

    Criação controller de Cliente

---
+++ imobiliaria/src/java/br/edu/ifnmg/imobiliaria/presentation/ClienteController.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.imobiliaria.presentation;
+
+import br.edu.ifnmg.imobiliaria.domainModel.Cliente;
+import br.edu.ifnmg.imobiliaria.domainModel.IClienteRepositorio;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author emerson
+ */
+@Named(value = "ClienteController")
+@RequestScoped
+public class ClienteController {
+
+    /**
+     * Creates a new instance of ClienteController
+     */
+    @EJB
+    IClienteRepositorio dao;
+    Cliente entidade;
+    Cliente filtro;
+    List<Cliente> listagem;
+    
+    public ClienteController() {
+        entidade = new Cliente();
+        filtro = new Cliente();
+    }
+    
+    public void exibirMensagem(String msg) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(msg));
+    }
+    
+    public void salvar(){
+        dao.Salvar(entidade);
+        listagem = null;
+        exibirMensagem("Salvo com sucesso!");
+    }
+    
+    public String editar(){
+        return "CadastroCliente.xhtml";
+    }
+    
+    public String criar(){
+        entidade = new Cliente();
+        return "CadastroCliente.xhtml";
+    }
+    
+    public String apagar(){
+        dao.Apagar(entidade);
+        listagem = null;
+        exibirMensagem("Apagado com sucesso!");
+        return "ListagemProfissao.xhtml";
+    }
+    
+    public String voltar(){
+        return "index.xhtml";
+    }
+
+    public IClienteRepositorio getDao() {
+        return dao;
+    }
+
+    public void setDao(IClienteRepositorio dao) {
+        this.dao = dao;
+    }
+
+    public Cliente getEntidade() {
+        return entidade;
+    }
+
+    public void setEntidade(Cliente entidade) {
+        this.entidade = entidade;
+    }
+
+    public Cliente getFiltro() {
+        return filtro;
+    }
+
+    public void setFiltro(Cliente filtro) {
+        this.filtro = filtro;
+    }
+
+    public List<Cliente> getListagem() {
+        return listagem;
+    }
+
+    public void setListagem(List<Cliente> listagem) {
+        this.listagem = listagem;
+    }
+    
+    
+    
+}
