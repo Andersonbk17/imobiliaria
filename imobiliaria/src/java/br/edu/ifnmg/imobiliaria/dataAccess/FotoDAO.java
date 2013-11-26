2f0589c9ad9c062438add49f54123357f6fe7c00
Anderson <Anderson@darkside>
2013-11-26 08:26:30 -0200

    Criacao de FotoDAO

---
+++ imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/FotoDAO.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.imobiliaria.dataAccess;
+
+import br.edu.ifnmg.imobiliaria.domainModel.Foto;
+import br.edu.ifnmg.imobiliaria.domainModel.IFoto;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Anderson
+ */
+@Stateless(name = "IFoto")
+public class FotoDAO extends DAOGenerico<Foto> implements IFoto{
+
+    public FotoDAO() {
+        super(Foto.class);
+    }
+
+    @Override
+    public List<Foto> Buscar(Foto obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean Apagar(Foto obj) {
+        try {
+
+            Query query = manager.createQuery("Update Foto s set s.ativo = 0 WHERE s.id :=id");
+            query.setParameter("id", obj.getId());
+            query.executeUpdate();
+
+            return true;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+            return false;
+        }
+    }
+    
+}
