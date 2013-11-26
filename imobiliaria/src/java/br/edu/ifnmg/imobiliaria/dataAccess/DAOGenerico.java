878fa3755603e19faed9d1e4ec5d6b7098b79e11
Anderson <Anderson@darkside>
2013-11-25 23:30:37 -0200

    Criacao da Classe DAOGenerico e unidade de persistencia

---
+++ imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/DAOGenerico.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.imobiliaria.dataAccess;
+
+import br.edu.ifnmg.imobiliaria.domainModel.IRepositorio;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Thaisa
+ */
+public abstract class DAOGenerico<T> implements IRepositorio<T>{
+    @PersistenceContext(name="imobiliariaPU")
+    protected EntityManager manager;
+    private Class tipo;
+    public DAOGenerico (Class t) {
+        tipo = t;
+    }
+    
+   
+
+ 
+    @Override
+    public boolean Salvar(T obj) {
+        try{
+            //salva o objeto
+            manager.merge(obj);
+            return true;
+        }catch (Exception ex){
+        System.out.println(ex.getMessage());
+        return false;
+        }
+    }
+    
+          
+    
+    @Override
+    public T Abrir(Long id) {
+        try {
+            T obj = (T) manager.find(tipo, id);
+            return obj;
+            //abrir
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+    
+    
+    @Override
+      public abstract List<T> Buscar(T obj);
+
+   
+    
+}
