b40121a86784ef4e9ecd6a0926f3cbe568eb0892
emerson <emerson@emerson-PC>
2013-11-27 23:15:00 -0200

    Criação converter Funcionario

---
+++ imobiliaria/src/java/br/edu/ifnmg/imobiliaria/presentation/FuncionarioConverter.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.imobiliaria.presentation;
+
+import br.edu.ifnmg.imobiliaria.domainModel.Funcionario;
+import br.edu.ifnmg.imobiliaria.domainModel.IFuncionarioRepositorio;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+/**
+ *
+ * @author emerson
+ */
+@Named(value = "funcionarioConverter")
+@SessionScoped
+public class FuncionarioConverter implements Serializable, Converter {
+
+    /**
+     * Creates a new instance of FuncionarioConverter
+     */
+    @EJB
+    IFuncionarioRepositorio dao;
+    
+    public FuncionarioConverter() {
+    }
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        if (value == null || value.trim().equals("")) {
+            return null;
+        } else {
+            Long id = Long.parseLong(value);
+            return dao.Abrir(id);
+        }
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        if (value == null || value.toString().equals("")){
+            return "";
+        } else{
+            Funcionario c = (Funcionario)value;
+            return c.getId().toString();
+        } 
+    }
+    
+}
