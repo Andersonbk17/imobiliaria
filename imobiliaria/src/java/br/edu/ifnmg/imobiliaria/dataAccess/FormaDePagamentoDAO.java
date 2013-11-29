89dc63b0395e15085418bf2ad87dcea6457a60d8
thaisa <thaisa@thaisa>
2013-11-28 23:41:17 -0200

    Alteraçaão dao Forma de pagamento

--- imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/FormaDePagamentoDAO.java
+++ imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/FormaDePagamentoDAO.java
@@ -39,7 +39,7 @@ public class FormaDePagamentoDAO extends DAOGenerico<FormaDePagamento> implement
         if (obj != null) {
             //Nome
             if (obj.getNome() != null && obj.getNome().length() > 0) {
-                filtro += " c.nome like nome ";
+                filtro += " c.nome=:nome ";
                 parametros.put("nome", obj.getNome());
             }
             //Id
@@ -47,7 +47,7 @@ public class FormaDePagamentoDAO extends DAOGenerico<FormaDePagamento> implement
                 if (filtro.length() > 0) {
                     filtro = filtro + " and ";
                 }
-                filtro += " c.id like id";
+                filtro += " c.id=:id ";
                 parametros.put("id", obj.getId());
             }
          
