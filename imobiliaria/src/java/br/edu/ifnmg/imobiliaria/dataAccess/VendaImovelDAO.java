07f76d52d1b6f0394d37ad29df17be512b591800
emerson <emerson@emerson-PC>
2013-12-03 17:13:44 -0200

    Alteração dao venda imovel

--- imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/VendaImovelDAO.java
+++ imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/VendaImovelDAO.java
@@ -8,6 +8,7 @@ package br.edu.ifnmg.imobiliaria.dataAccess;
 
 import br.edu.ifnmg.imobiliaria.domainModel.IVendaImovelRepositorio;
 import br.edu.ifnmg.imobiliaria.domainModel.VendaImovel;
+import java.util.HashMap;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.Query;
@@ -25,7 +26,42 @@ public class VendaImovelDAO extends DAOGenerico<VendaImovel> implements IVendaIm
 
     @Override
     public List<VendaImovel> Buscar(VendaImovel obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         // Corpo da consulta
+        String consulta = "select c from VendaImovel c";
+
+        // A parte where da consulta
+        String filtro = "";
+
+        // Guarda a lista de parâmetros da query
+        HashMap<String, Object> parametros = new HashMap<String, Object>();
+
+        // Verifica campo por campo os valores que serão filtrados
+        if (obj != null) {
+            //Id
+            if (obj.getId() != null && obj.getId() > 0) {
+                if (filtro.length() > 0) {
+                    filtro = filtro + " and ";
+                }
+                filtro += " c.id=:id ";
+                parametros.put("id", obj.getId());
+            }
+         
+            // Se houver filtros, coloca o "where" na consulta
+            if (filtro.length() > 0) {
+                consulta = consulta + " where " + filtro;
+            }
+        }
+
+        // Cria a consulta no JPA
+        Query query = manager.createQuery(consulta);
+
+        // Aplica os parâmetros da consulta
+        for (String par : parametros.keySet()) {
+            query.setParameter(par, parametros.get(par));
+        }
+
+        // Executa a consulta
+        return query.getResultList();
     }
 
     @Override
