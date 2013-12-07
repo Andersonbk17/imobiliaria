169ac416fe8c01ab77332801fe1df5c1dcb09901
emerson <emerson@emerson-PC>
2013-12-07 20:10:50 -0200

    Alteração metodo apagar na Dao interessado

--- imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/InteressadoDAO.java
+++ imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/InteressadoDAO.java
@@ -73,7 +73,7 @@ public class InteressadoDAO extends DAOGenerico<Interessado> implements IInteres
     @Override
     public boolean Apagar(Interessado obj) {
        try {
-            Query query = manager.createQuery("Update Interessado s set s.ativo = 0 WHERE s.id :=id");
+            Query query = manager.createQuery("Update Interessado s set s.ativo = 0 WHERE s.id =:id");
             query.setParameter("id", obj.getId());
             query.executeUpdate();
 
