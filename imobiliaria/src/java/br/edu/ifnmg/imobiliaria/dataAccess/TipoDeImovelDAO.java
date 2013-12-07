e33c82e787dfeaf64ee78cd5ec8511194d4789f9
emerson <emerson@emerson-PC>
2013-12-07 20:11:53 -0200

    Alteração metodo apagar na Dao tipo de imovel

--- imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/TipoDeImovelDAO.java
+++ imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/TipoDeImovelDAO.java
@@ -64,7 +64,7 @@ public class TipoDeImovelDAO extends DAOGenerico<TipoDeImovel> implements ITipoD
      
         try {
 
-            Query query = manager.createQuery("Update TipoDeImovel s set s.ativo = 0 WHERE s.id :=id");
+            Query query = manager.createQuery("Update TipoDeImovel s set s.ativo = 0 WHERE s.id =:id");
             query.setParameter("id", obj.getId());
             query.executeUpdate();
 
