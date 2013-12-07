ee16a4ebd769134758b6c1d591c7a31a87bbd6dc
emerson <emerson@emerson-PC>
2013-12-07 20:13:09 -0200

    Alteração metodo apagar na Dao visita

--- imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/VisitaDAO.java
+++ imobiliaria/src/java/br/edu/ifnmg/imobiliaria/dataAccess/VisitaDAO.java
@@ -74,7 +74,7 @@ public class VisitaDAO extends DAOGenerico<Visita> implements IVisitaRepositorio
     public boolean Apagar(Visita obj) {
         try {
 
-            Query query = manager.createQuery("Update Visita s set s.ativo = 0 WHERE s.id:=id");
+            Query query = manager.createQuery("Update Visita s set s.ativo = 0 WHERE s.id =:id");
             query.setParameter("id", obj.getId());
             query.executeUpdate();
 
